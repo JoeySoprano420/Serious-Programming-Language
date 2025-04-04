bc4dfc9d42d7c5d4b01eefb8e5e890f62f23388d
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-04-04 17:06:59 -0400

    Update Compiler.cpp

--- Compiler.cpp
+++ Compiler.cpp
@@ -95,6 +95,7 @@ private:
     void processLine(const std::string& line) {
         if (macros.find(line) != macros.end()) {
             macros[line]();
+
         } else if (line.find("print") != std::string::npos) {
             auto start = line.find("\"") + 1;
             auto end = line.rfind("\"");
@@ -108,16 +109,22 @@ private:
         } else if (line.find("var") == 0) {
             std::string name;
             int val;
-            sscanf(line.c_str(), "var %s = %d", &name[0], &val);
+            size_t nameStart = 4;
+            size_t eqPos = line.find("=");
+            name = line.substr(nameStart, eqPos - nameStart - 1);
+            val = std::stoi(line.substr(eqPos + 1));
             variables[name] = val;
 
-        } else if (line.find("+") != std::string::npos || line.find("-") != std::string::npos) {
+        } else if (line.find("=") != std::string::npos && (line.find("+") != std::string::npos || line.find("-") != std::string::npos || line.find("*") != std::string::npos || line.find("/") != std::string::npos)) {
             evaluateArithmetic(line);
 
         } else if (line.find("buffer create") != std::string::npos) {
             std::string name;
             int size;
-            sscanf(line.c_str(), "buffer create %s %d", &name[0], &size);
+            size_t nameStart = 14;
+            size_t spacePos = line.find(" ", nameStart);
+            name = line.substr(nameStart, spacePos - nameStart);
+            size = std::stoi(line.substr(spacePos + 1));
             memoryManager.createBuffer(name, size);
 
         } else if (line.find("buffer write") != std::string::npos) {
@@ -154,10 +161,15 @@ private:
     }
 
     void evaluateArithmetic(const std::string& line) {
-        char var[64];
-        int lhs, rhs;
+        std::string var;
         char op;
-        sscanf(line.c_str(), "%s = %d %c %d", var, &lhs, &op, &rhs);
+        int lhs, rhs;
+        size_t eqPos = line.find("=");
+        var = line.substr(0, eqPos - 1);
+
+        std::istringstream iss(line.substr(eqPos + 1));
+        iss >> lhs >> op >> rhs;
+
         switch (op) {
             case '+': variables[var] = lhs + rhs; break;
             case '-': variables[var] = lhs - rhs; break;
@@ -171,7 +183,8 @@ private:
         std::string var;
         char cmp;
         int value;
-        sscanf(cond.c_str(), "%s %c %d", &var[0], &cmp, &value);
+        std::istringstream iss(cond);
+        iss >> var >> cmp >> value;
         if ((cmp == '>' && variables[var] > value) ||
             (cmp == '<' && variables[var] < value) ||
             (cmp == '=' && variables[var] == value)) {
@@ -222,19 +235,268 @@ print "Welcome to SuperCodeX!"
 var x = 10
 x = 10 + 5
 if x > 5 {
-print "Greater than five!"
+print "X is greater than 5"
 }
-buffer create myBuf 128
-buffer write myBuf HelloWorld
-buffer delete myBuf
-tunnel send HelloTunnels
+buffer create mybuf 32
+buffer write mybuf HelloWorld!
+tunnel send PingMessage
 tunnel receive
-gui map ∑ add
+gui map + add
+gui map - subtract
 |> thread_async
 )";
 
     SeriousCompiler compiler;
-    std::cout << "Compiling and running SuperCodeX Level 3...\n";
     compiler.compileAndRun(code);
     return 0;
 }
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <functional>
+#include <thread>
+#include <future>
+#include <vector>
+#include <filesystem>
+#include <fstream>
+#include <chrono>
+#include <sstream>
+#include <mutex>
+#include <queue>
+#include <condition_variable>
+#include <unordered_map>
+#include <atomic>
+#include <cctype>
+#include <stack>
+
+// VirtuWeb CLI Mapping Placeholder (Replaces GUI)
+class VirtuWebCLI {
+public:
+    void mapSymbol(const std::string& symbol, const std::string& operation) {
+        std::cout << "VirtuWebCLI: " << symbol << " => " << operation << std::endl;
+    }
+};
+
+class MemoryManager {
+    std::unordered_map<std::string, std::vector<char>> buffers;
+public:
+    void createBuffer(const std::string& name, size_t size) {
+        buffers[name] = std::vector<char>(size);
+        std::cout << "Buffer '" << name << "' created with size: " << size << std::endl;
+    }
+
+    void writeBuffer(const std::string& name, const std::string& data) {
+        if (buffers.find(name) != buffers.end()) {
+            size_t len = std::min(data.size(), buffers[name].size());
+            std::copy(data.begin(), data.begin() + len, buffers[name].begin());
+            std::cout << "Buffer '" << name << "' written." << std::endl;
+        }
+    }
+
+    void deleteBuffer(const std::string& name) {
+        buffers.erase(name);
+        std::cout << "Buffer '" << name << "' deallocated." << std::endl;
+    }
+};
+
+class SplicingTunnel {
+    std::queue<std::string> dataQueue;
+    std::mutex mtx;
+    std::condition_variable cv;
+public:
+    void send(const std::string& data) {
+        std::unique_lock<std::mutex> lock(mtx);
+        dataQueue.push(data);
+        cv.notify_one();
+    }
+
+    std::string receive() {
+        std::unique_lock<std::mutex> lock(mtx);
+        cv.wait(lock, [this]() { return !dataQueue.empty(); });
+        std::string data = dataQueue.front();
+        dataQueue.pop();
+        return data;
+    }
+};
+
+class SeriousCompiler {
+private:
+    std::map<std::string, std::function<void()>> macros;
+    std::map<std::string, std::function<void()>> functions;
+    bool condition = true;
+    MemoryManager memoryManager;
+    SplicingTunnel tunnel;
+    VirtuWebCLI cliMapper;
+    std::map<std::string, int> variables;
+    std::queue<std::function<void()>> retryQueue;
+
+public:
+    SeriousCompiler() {
+        macros["@macro init_boot"] = []() {
+            std::cout << "Boot initialized." << std::endl;
+        };
+    }
+
+    void compileAndRun(const std::string& code) {
+        std::istringstream stream(code);
+        std::string line;
+        while (std::getline(stream, line)) {
+            processLine(line);
+        }
+        while (!retryQueue.empty()) {
+            auto task = retryQueue.front();
+            retryQueue.pop();
+            task();
+        }
+    }
+
+private:
+    void processLine(const std::string& line) {
+        try {
+            if (macros.find(line) != macros.end()) {
+                macros[line]();
+
+            } else if (line.find("print") != std::string::npos) {
+                auto start = line.find("\"") + 1;
+                auto end = line.rfind("\"");
+                std::cout << "Output: " << line.substr(start, end - start) << std::endl;
+
+            } else if (line.find("if") == 0 && line.find("{") != std::string::npos) {
+                size_t varPos = line.find(" ") + 1;
+                std::string conditionExpr = line.substr(varPos, line.find("{") - varPos);
+                evaluateCondition(conditionExpr);
+
+            } else if (line.find("var") == 0) {
+                std::string name;
+                int val;
+                size_t nameStart = 4;
+                size_t eqPos = line.find("=");
+                name = line.substr(nameStart, eqPos - nameStart - 1);
+                val = std::stoi(line.substr(eqPos + 1));
+                variables[name] = val;
+
+            } else if (line.find("=") != std::string::npos &&
+                       (line.find("+") != std::string::npos || line.find("-") != std::string::npos ||
+                        line.find("*") != std::string::npos || line.find("/") != std::string::npos)) {
+                evaluateArithmetic(line);
+
+            } else if (line.find("buffer create") != std::string::npos) {
+                std::string name;
+                int size;
+                size_t nameStart = 14;
+                size_t spacePos = line.find(" ", nameStart);
+                name = line.substr(nameStart, spacePos - nameStart);
+                size = std::stoi(line.substr(spacePos + 1));
+                memoryManager.createBuffer(name, size);
+
+            } else if (line.find("buffer write") != std::string::npos) {
+                std::string name, data;
+                size_t nameStart = line.find(" ", 13) + 1;
+                size_t nameEnd = line.find(" ", nameStart);
+                name = line.substr(nameStart, nameEnd - nameStart);
+                data = line.substr(nameEnd + 1);
+                memoryManager.writeBuffer(name, data);
+
+            } else if (line.find("buffer delete") != std::string::npos) {
+                std::string name = line.substr(14);
+                memoryManager.deleteBuffer(name);
+
+            } else if (line.find("tunnel send") != std::string::npos) {
+                std::string data = line.substr(12);
+                tunnel.send(data);
+
+            } else if (line.find("tunnel receive") != std::string::npos) {
+                std::cout << "Tunnel received: " << tunnel.receive() << std::endl;
+
+            } else if (line.find("cli map") != std::string::npos) {
+                size_t delim = line.find(" ", 8);
+                std::string sym = line.substr(8, delim - 8);
+                std::string op = line.substr(delim + 1);
+                cliMapper.mapSymbol(sym, op);
+
+            } else if (line.find("func ") == 0) {
+                size_t nameStart = 5;
+                std::string name = line.substr(nameStart);
+                std::string body;
+                std::getline(std::cin, body);
+                functions[name] = [body, this]() { this->processLine(body); };
+
+            } else if (line.find("run ") == 0) {
+                std::string name = line.substr(4);
+                if (functions.find(name) != functions.end()) {
+                    functions[name]();
+                }
+
+            } else if (line.find("|> thread_async") != std::string::npos) {
+                executeRealAsyncTasks();
+
+            } else {
+                std::cout << "Unrecognized line: " << line << std::endl;
+            }
+        } catch (...) {
+            retryQueue.push([line, this]() { processLine(line); });
+            std::cout << "Error deferred, will retry: " << line << std::endl;
+        }
+    }
+
+    void evaluateArithmetic(const std::string& line) {
+        std::string var;
+        char op;
+        int lhs, rhs;
+        size_t eqPos = line.find("=");
+        var = line.substr(0, eqPos - 1);
+
+        std::istringstream iss(line.substr(eqPos + 1));
+        iss >> lhs >> op >> rhs;
+
+        switch (op) {
+            case '+': variables[var] = lhs + rhs; break;
+            case '-': variables[var] = lhs - rhs; break;
+            case '*': variables[var] = lhs * rhs; break;
+            case '/': variables[var] = rhs != 0 ? lhs / rhs : 0; break;
+        }
+        std::cout << var << " = " << variables[var] << std::endl;
+    }
+
+    void evaluateCondition(const std::string& cond) {
+        std::string var;
+        char cmp;
+        int value;
+        std::istringstream iss(cond);
+        iss >> var >> cmp >> value;
+        if ((cmp == '>' && variables[var] > value) ||
+            (cmp == '<' && variables[var] < value) ||
+            (cmp == '=' && variables[var] == value)) {
+            std::cout << "Condition met: " << cond << std::endl;
+        } else {
+            std::cout << "Condition failed: " << cond << std::endl;
+        }
+    }
+
+    void executeRealAsyncTasks() {
+        std::cout << "Starting async tasks..." << std::endl;
+
+        auto task1 = std::async(std::launch::async, []() {
+            std::this_thread::sleep_for(std::chrono::seconds(2));
+            std::cout << "Task 1: Fetched data from URL\n";
+        });
+
+        auto task2 = std::async(std::launch::async, []() {
+            std::filesystem::path testPath("example.txt");
+            if (std::filesystem::exists(testPath)) {
+                std::cout << "Task 2: Path validation successful\n";
+            } else {
+                std::cout << "Task 2: Path does not exist\n";
+            }
+        });
+
+        auto task3 = std::async(std::launch::async, []() {
+            std::ifstream file("example.txt", std::ios::binary);
+            if (file.is_open()) {
+                std::cout << "Task 3: CRC...";
+            }
+        });
+    }
+};
+
